23364c02c970e99e76063c57da9439d0a5bf18a7
Koushik Dey <koushik.dey@live.com>
2023-02-28 17:43:34 +0200

    shared dependencies between apps

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,5 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
-
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 
 console.log('container');
+
+productsMount(document.querySelector('#dev-products'));
+cartMount(document.querySelector('#cart-dev'));
